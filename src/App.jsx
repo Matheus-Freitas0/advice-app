3edfd4ad7916344eb1fb5b50fd6b9043d664c5b6
MatheusFreitas <moncaomatheus@gmail.com>
2024-09-23 16:03:27 -0300

    Updated index.html title and refactored App.jsx to fetch and display random advice

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import './App.css'
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-     <h1 className="text-red-400 text-center text-3xl">Tailwind CSS</h1>
+  const [advice, setAdvice] = useState('');
+  const [adviceId, setAdviceId] = useState('')
 
-  )
+  const fetchAdvice = async () => {
+
+    try {
+      const randomNumber = Math.floor(Math.random() * 100);
+      const response = await fetch(`https://api.adviceslip.com/advice/${randomNumber}`);
+      const data = await response.json();
+      setAdvice(data.slip.advice);
+      setAdviceId(data.slip.id);
+
+    } catch (error) {
+
+      console.error("Erro ao buscar conselho:", error);
+
+    }
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-80text-center">
+        <span className="text-gray-500 text-sm">Conselho #{adviceId}</span>
+        <p className="text-lg font-semibold my-4">{advice}</p>
+        <button
+          onClick={fetchAdvice}
+          className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600 transition duration-300"
+        >
+          Gerar Novo Conselho
+        </button>
+      </div>
+    </div>
+  );
 }
-
-export default App
+export default App;
